81f21a381771939736c33ad20a73604ca6d2ed98
Simmy <simmy@simmybot.com>
2017-06-06 22:06:39 -0400

    Color Logging.

--- classes/zip.py
+++ classes/zip.py
@@ -1,9 +1,19 @@
-import sys, os, zipfile
-
+import sys, os, zipfile, time
+from colorama import init
+init()
 
 class Zipped:
+	def log(self,msg,Color):
+		currenttime = time.strftime("%H:%M:%S")
+		sys.stdout.write("[%s] %s\n" % (currenttime, Color + (msg) ))
+		sys.stdout.flush()
 
 	def zipdir(self,zipname, dirname):
+		underline = '\033[04m'
+		lightblue = '\033[94m'
+		red = '\033[31m'
+		Green = '\033[32m'
+		Reset = '\033[0m'
 		total = 0
 		for root, dirs, files in os.walk(dirname):
 			for fname in files:
@@ -17,8 +27,7 @@ class Zipped:
 				path = os.path.join(root, fname)
 				arcname = os.path.join(basename, fname)
 				percent = 100 * current / total
-				print '%3d%% %s' % (percent, path)
-
+				self.log('%3d %s' % (percent, path), Green)
 				z.write(path, arcname)
 				current += os.path.getsize(path)
 		z.close()
